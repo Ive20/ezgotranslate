196a49d25b5d4c207a6e126d1cf367005ddfc220
reimondo <reimondo.zero@gmail.com>
2014-07-25 01:48:08 +0800

    login via jQuery

--- public/web/script/signinup.js
+++ public/web/script/signinup.js
@@ -2,7 +2,6 @@
     $(".container_in .switch").click(function () {
         $(".container_in").attr("style", "display:none;");
         $(".container_up").attr("style", "display:block;");
-       
     });
     $(".container_up .switch").click(function () {
         $(".container_up").attr("style", "display:none;");
@@ -17,7 +16,29 @@
     });
     $(".si").click(function () {
         $(".container_in").attr("style", "display:block;");
-        
     });
     
+
+    /*Submit*/
+    $("#signin").click(function () {
+        var login = $("div#signin-windows input");
+        var acc = login[0].value;
+        var pas = login[1].value;
+
+        $.post("/user/login",
+            {
+                username: acc,
+                password: pas
+            },
+        function(data){
+            var errcode = data.errcode;
+            if (errcode === 1) {
+                $("#signin").after("<span>登陆失败</span>");
+            } else if (errcode === 0) {
+                //跳转到会员界面
+            }
+        },
+        "json");
+        //alert(acc + "\n" + pas);
+    })
 });
