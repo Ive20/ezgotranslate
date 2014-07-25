d144ac8017c5ddd46e82f1504fc705eda72780a8
reimondo <reimondo.zero@gmail.com>
2014-07-25 14:48:54 +0800

    register via jQuery

--- public/web/script/signinup.js
+++ public/web/script/signinup.js
@@ -19,9 +19,11 @@
     });
     
 
-    /*Submit*/
+    /*login*/
     $("#signin").click(function () {
-        var login = $("div#signin-windows input");
+        //function checkForm(){}
+        //var login = $("div#signin-windows input");
+        var login = $(".container_in #signin-windows input");
         var acc = login[0].value;
         var pas = login[1].value;
 
@@ -30,15 +32,43 @@
                 username: acc,
                 password: pas
             },
-        function(data){
-            var errcode = data.errcode;
-            if (errcode === 1) {
-                $("#signin").after("<span>登陆失败</span>");
-            } else if (errcode === 0) {
-                //跳转到会员界面
-            }
+            function(data){
+                var errcode = data.errcode;
+               if (errcode === 1) {
+                    $("#signin").after("<span>登陆失败</span>");
+              } else if (errcode === 0) {
+                  //跳转到会员界面
+               }
         },
         "json");
-        //alert(acc + "\n" + pas);
+        alert(acc + "\n" + pas);
     })
+
+    /*register*/
+    $("#signup").click(function () {
+        //checkForm(){}
+        //var register = $("div#signup-windows input");
+        var register = $(".container_up #signin-windows input");
+        var acc = register[0].value;
+        var email = register[1].value;
+        var pas = register[3].value;
+
+        $.post("/user/register",
+            {
+                username: acc,
+                password: pas,
+                email: email
+            },
+            function (data) {
+                var errcode = data.errcode;
+                if (errcode === 1) {
+                    $("#signup").after("<span>注册失败</span>");
+                } else if (errcode === 1) {
+                    //跳转到会员界面
+                }
+            },
+            "json")
+        //Test
+        alert(acc + "\n" + pas + "\n" + email);
+    });
 });
