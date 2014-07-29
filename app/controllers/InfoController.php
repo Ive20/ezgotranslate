0f0a9d4b6f629d2674ecc20083de6eb2abb01ff6
maxwell <lxhomj@gmail.com>
2014-07-29 20:53:55 +0800

    add infoCOntroller

---
+++ app/controllers/InfoController.php
@@ -0,0 +1,27 @@
+<?php
+
+class InfoController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+	public function getInfo()
+	{
+		return View::make('hello');
+	}
+	public function getIndex()
+	{
+		return 'hello';
+	}
+
+}
