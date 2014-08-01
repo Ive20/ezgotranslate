ee37234f0e01b75e933f237c275b473a1cc2678d
reimondo <reimondo.zero@gmail.com>
2014-08-02 00:38:40 +0800

    Debug

--- public/web/script/signinup.js
+++ public/web/script/signinup.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function(){
     $(".container_in .switch").click(function () {
         $(".container_in").attr("style", "display:none;");
         $(".container_up").attr("style", "display:block;");
@@ -17,52 +17,51 @@
     $(".si").click(function () {
         $(".container_in").attr("style", "display:block;");
     });
-    
+
 
     /*login*/
-    /*
     $("#signin").click(function () {
         //function checkForm(){}
-        //var login = $("div#signin-windows input");
         var login = $(".container_in #signin-windows input");
         var acc = login[0].value;
         var pas = login[1].value;
 
-        $.post("/user/login",
+        var jqxhr = $.post("/user/login",
             {
                 username: acc,
                 password: pas
             },
-            function(data){
+            function (data, status) {
+                alert("Status: " + status);
                 var errcode = data.errcode;
-               if (errcode === 1) {
+                if (errcode === 1) {
                     $("#signin").after("<span>登陆失败</span>");
-              } else if (errcode === 0) {
-                  //跳转到会员界面
-                  alert("Success");
-               }
-        },
+                } else if (errcode === 0) {
+                    //跳转到会员界面
+                    alert("Success");
+                }
+            },
         "json");
+        jqxhr.fail(function () { alert("Request fail!"); });
         //alert(acc + "\n" + pas);
     })
-    */
+
     /*register*/
-    /*
     $("#signup").click(function () {
         //checkForm(){}
-        //var register = $("div#signup-windows input");
         var register = $(".container_up #signin-windows input");
         var acc = register[0].value;
         var email = register[1].value;
         var pas = register[3].value;
 
-        $.post("/user/register",
+        var jqxhr = $.post("/user/register",
             {
                 username: acc,
                 password: pas,
                 email: email
             },
-            function (data) {
+            function (data, status) {
+                alert("Status: " + status);
                 var errcode = data.errcode;
                 if (errcode === 1) {
                     $("#signup").after("<span>注册失败</span>");
@@ -71,9 +70,9 @@
                     alert("Success");
                 }
             },
-            "json")
+            "json");
+        jqxhr.fail(function () { alert("Request fail!") });
         //Test
         //alert(acc + "\n" + pas + "\n" + email);
     });
-    */
-});
+})
