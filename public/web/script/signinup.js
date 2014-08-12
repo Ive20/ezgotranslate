ef0197113a77ee1e9e51bb2e626e35f624d67401
reimondo <reimondo.zero@gmail.com>
2014-08-12 23:14:35 +0800

    Test login and register Jump

--- public/web/script/signinup.js
+++ public/web/script/signinup.js
@@ -17,12 +17,29 @@
     $(".si").click(function () {
         $(".container_in").attr("style", "display:block;");
     });
-
-
-    /*login*/
+    /* Create formStatus Object to record the info. filled
+     * IF info. wrong value = false
+     * ELSE value = true
+     */
+    /* $begin formStatus */
+    function formStatus() {
+        this.acc = true;
+        this.pass = true;
+        this.email = true;
+        this.remind = true;
+    }
+    /* $end formStatus */
+    var signInForm = new formStatus();
+    var signUpForm = new formStatus();
+    /* Login */
     $("#signin").click(function () {
-        //function checkForm(){}
         var login = $(".container_in #signin-windows input");
+        /* Clear error remind info. */
+        if (!signInForm.remind) {
+            $(".container_in #remove").remove();
+            signInForm.remind = true;
+        }
+        //function checkForm(){}
         var acc = login[0].value;
         var pas = login[1].value;
         var jqxhr = $.post("/user/login",
@@ -31,13 +48,15 @@
                 password: pas
             },
             function (data, status) {
-                alert("Status: " + status + "\nData: " + data);
+                //alert("Status: " + status + "\nData: " + data);
                 var errcode = data.errcode;
-                alert(errcode);
+                //alert(errcode);
                 if (errcode == 1) {
-                    $("#signin").after("<span>登陆失败</span>");
+                    $("#signin").after("<span id = \"remove\">登陆失败</span>");
+                    signInForm.remind = false;
                 } else if (errcode == 0) {
-                    //跳转到会员界面
+                    /* Jump to private page */
+                    $(".container_in").attr("style", "display: none;");
                     alert("Success");
                 }
             },
@@ -46,10 +65,15 @@
         //alert(acc + "\n" + pas);
     })
 
-    /*register*/
+    /* Register */
     $("#signup").click(function () {
-        //checkForm(){}
         var register = $(".container_up #signin-windows input");
+        /* Clear error remind info. */
+        if (!signUpForm.remind) {
+            $(".container_up #remove").remove();
+            signUpForm.remind = true;
+        }
+        //checkForm(){}
         var acc = register[0].value;
         var email = register[1].value;
         var pas = register[3].value;
@@ -65,15 +89,15 @@
                 var errcode = data.errcode;
                 alert(errcode);
                 if (errcode == 1) {
-                    $("#signup").after("<span>注册失败</span>");
+                    $("#signup").after("<span id = \"remove\">注册失败</span>");
+                    signUpForm.remind = false;
                 } else if (errcode == 0) {
-                    //跳转到会员界面
+                    /* Jump to private page */
+                    $(".container_up").attr("style", "display: none");
                     alert("Success");
                 }
             },
             "json");
         jqxhr.fail(function () { alert("Request fail!") });
-        //Test
-        //alert(acc + "\n" + pas + "\n" + email);
     });
 })
