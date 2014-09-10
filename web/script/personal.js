65cc8c5c9d19ed8a3b78a98a81e3d0593fb12798
Boby <zsb0213@gmail.com>
2014-09-10 22:41:37 +0800

    start to work

---
+++ web/script/personal.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $("#personal").hover(function () {
+        $("#personal ul").stop().slideDown();
+    }
+    , function () {
+        $("#personal ul").stop().slideUp("fast");
+    });
+});
