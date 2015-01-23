117f58e9a3b4b2f7eea3e5c5760e118e8fc75c8f
reimondo <reimondo.zero@gmail.com>
2015-01-23 10:16:28 +0800

    Add template.js and add ajax template

---
+++ public/web/js/template.js
@@ -0,0 +1,34 @@
+//
+// Ajax
+//
+$.ajax({
+    url: '/path/to/file',
+    type: 'default GET (Other values: POST)',
+    dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+    data: {param1: 'value1'},
+})
+.done(function() {
+    console.log("success");
+})
+.fail(function() {
+    console.log("error");
+})
+.always(function() {
+    console.log("complete");
+});
+
+$.post('/path/to/file', param1: 'value1', function(data, textStatus, xhr) {
+    /*optional stuff to do after success */
+});
+
+// Post json data using $.post
+$.post(
+    '/path/to/file',
+    {
+        param1: 'value1',
+    }
+    function(data, textStatus, xhr) {
+    /*optional stuff to do after success */
+    },
+    'json'
+);
