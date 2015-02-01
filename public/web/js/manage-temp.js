79ea368ef1d1416656314824af9a92a4c8e7d985
Boby <zsb0213@gmail.com>
2015-02-02 00:16:44 +0800

    Add manage.htm to upload file

---
+++ public/web/js/manage-temp.js
@@ -0,0 +1,19 @@
+﻿
+
+document.onready = function () {
+    var realInput = document.getElementById("real-input");
+    var fakeInput = document.getElementById("fake-input");
+
+    realInput.onchange = function () {
+        
+        var url = realInput.files[0].name;
+        fakeInput.innerHTML = url;
+    }
+
+
+    realInput.files[0];
+    var upload = document.getElementsByClassName("upload")[0];
+
+
+    
+}
