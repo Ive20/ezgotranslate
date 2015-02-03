0fd133c062015ca675bbb1be95dcb66df75bc2f6
Boby <zsb0213@gmail.com>
2015-02-03 13:41:59 +0800

    File upload interface ---- Done!

--- public/web/js/manage-temp.js
+++ public/web/js/manage-temp.js
@@ -1,9 +1,10 @@
 ﻿
+// real-input.onchange = 
 function fileSelected() {
     var file = document.getElementById("real-input").files[0];
 
     if (file) {
-        var fileSize = 0;
+        var fileSize = 0;  // fileSize = the sizw of file
         if (file.size > 1024 * 1024) {
             fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
         }
@@ -17,55 +18,70 @@ function fileSelected() {
     }
 }
 
+// upload.onclick =
 function uploadFile() {
+    file = document.getElementById("real-input").files[0];
+    
+    // if didn't choose any file or choose wrong type file
+    if (file == undefined) {
+        alert("请选择正确的文件，再确认上传！");
+        return ;
+    }
+
     var fd = new FormData();
-    fd.append("fileToUplaod", document.getElementById("real-input").files[0]);
+    fd.append("translate", file);
 
     var xhr = new XMLHttpRequest();
-    //xhr.upload.addEventListener("progress", uploadProgress, false);
-    //xhr.addEventListener("load", uploadComplete, false);
-    //xhr.addEventListener("error", uploadFailed, false);
-    //xhr.addEventListener("abort", uploadCanceled, false);
-
-    xhr.onload = function (event) {
-        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-            document.getElementById("upload-status").innerHTML = "上传状态： 上传成功！";
-            alert(event.errcode);
-        }
-    };
+    xhr.upload.addEventListener("progress", uploadProgress, false);
+    xhr.addEventListener("load", uploadComplete, false);
+    xhr.addEventListener("error", uploadFailed, false);
+    xhr.addEventListener("abort", uploadCanceled, false);
 
     xhr.open("POST", "/translate/uploadtranslate");
     xhr.send(fd);
 }
 
+// addEventListener for progress
 function uploadProgress(event) {
     if (event.lengthComputable) {
-        var percentComplete = Math.round(event.loaded * 100 / event.tottal);
-
+        // percentComplete use to mark the percente of uploading file
+        var percentComplete = Math.round(event.loaded * 100 / event.total);
+        // change style
         document.getElementById("upload-status").innerHTML = '上传状态： 已上传 ' + percentComplete.toString() + '%';
     }
-
     else {
         document.getElementById("upload-status").innerHTML = "未能获取到文件大小";
     }
 }
 
+// addEventListener for load
 function uploadComplete(event) {
-    //alert(event.errcode);
-    if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304)
+    // get responseText
+    var responJSON = JSON.parse(this.responseText);
+
+    // highlight upload result
+    document.getElementById("upload-status").style.color = 'red';
+
+    if (responJSON.errcode == 0) {
         document.getElementById("upload-status").innerHTML = "上传状态： 上传成功！";
+    }
+    else {
+        document.getElementById("upload-status").innerHTML = "上传状态： 上传失败！";
+    }
 }
 
+// addEventListener for error
 function uploadFailed(envent) {
     document.getElementById("upload-status").innerHTML = "上传状态：上传失败，请稍后重试！";
 }
 
+// addEventListener for abort
 function uploadCanceled(event) {
     document.getElementById("upload-status").innerHTML = "上传状态：上传已被取消。";
 }
 
 /*********************************************************
-   Start from here
+   Upload file start from here
  *********************************************************/
 document.onready = function () {
     var realInput = document.getElementById("real-input");
@@ -79,44 +95,8 @@ document.onready = function () {
         fileSelected();
     }
 
-
-    realInput.files[0];
     var upload = document.getElementsByClassName("upload")[0];
-
     upload.onclick = function () {
-        file = document.getElementById("real-input").files[0];
-
-        var fd = new FormData();
-        fd.append("fileToUplaod", document.getElementById("real-input").files[0]);
-
-        var xhr = new XMLHttpRequest();
-        //xhr.upload.addEventListener("progress", uploadProgress, false);
-        //xhr.addEventListener("load", uploadComplete, false);
-        //xhr.addEventListener("error", uploadFailed, false);
-        //xhr.addEventListener("abort", uploadCanceled, false);
-
-        xhr.onload = function (event) {
-            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-                document.getElementById("upload-status").innerHTML = "上传状态： 上传成功！";
-                alert(xhr.response);
-            }
-        };
-
-        xhr.upload.onprogress = function(event) {
-            if (event.lengthComputable) {
-                var percentComplete = Math.round(event.loaded * 100 / event.total);
-
-                document.getElementById("upload-status").innerHTML = '上传状态： 已上传 ' + percentComplete.toString() + '%';
-            }
-
-            else {
-                document.getElementById("upload-status").innerHTML = "未能获取到文件大小";
-            }
-        }
-
-        xhr.open("POST", "../translate/uploadtranslate", true);
-        xhr.setRequestHeader('Content-Type', file.type);
-        xhr.send(file);
-    };
-    
+        uploadFile();
+    }
 }
