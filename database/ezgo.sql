866e9eee4097de1b6c1bde5e2a137bf3ecca7672
maxwell <lxhomj@gmail.com>
2015-03-15 16:19:20 +0800

    change database

--- database/ezgo.sql
+++ database/ezgo.sql
@@ -18,28 +18,6 @@ USE `ezgotranslate`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `groups`
---
-
-DROP TABLE IF EXISTS `groups`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `groups` (
-  `group_id` int(11) NOT NULL,
-  PRIMARY KEY (`group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `groups`
---
-
-LOCK TABLES `groups` WRITE;
-/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `infos`
 --
 
@@ -47,14 +25,15 @@ DROP TABLE IF EXISTS `infos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `infos` (
-  `info_id` varchar(128) NOT NULL DEFAULT '',
-  `info_content` text,
-  `info_language` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `language` varchar(45) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`info_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,32 +42,141 @@ CREATE TABLE `infos` (
 
 LOCK TABLES `infos` WRITE;
 /*!40000 ALTER TABLE `infos` DISABLE KEYS */;
-INSERT INTO `infos` VALUES ('14104441145411ab52054d23.99321960','',NULL,'2014-09-20 03:40:14',NULL,'2014-09-20 03:40:14'),('141044610615411b31aab1fd','hahaha','en','2014-10-14 03:40:51','2014-09-11 06:35:06','2014-10-14 03:40:51'),('141068563354155ac1a9da31.91101597','hi','en','2014-09-14 01:07:13','2014-09-14 01:07:13',NULL),('1413286885543d0be54de0b8.51770617','hehe','en','2014-10-14 03:41:25','2014-10-14 03:41:25',NULL);
+INSERT INTO `infos` VALUES (1,'你好','chinese',1,'2015-03-12 22:01:36','2015-03-12 22:01:10','2015-03-12 22:01:36');
 /*!40000 ALTER TABLE `infos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `role`
+-- Table structure for table `permission_role`
 --
 
-DROP TABLE IF EXISTS `role`;
+DROP TABLE IF EXISTS `permission_role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `role` (
-  `roles_id` varchar(128) NOT NULL,
-  `role_name` varchar(128) NOT NULL,
-  PRIMARY KEY (`roles_id`)
+CREATE TABLE `permission_role` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `permission_id` int(10) NOT NULL,
+  `role_id` int(10) NOT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `permission_role_permission_id_foreign` (`permission_id`),
+  KEY `permission_role_role_id_foreign` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permission_role`
+--
+
+LOCK TABLES `permission_role` WRITE;
+/*!40000 ALTER TABLE `permission_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permission_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permissions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `display_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `permissions_name_unique` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `projects_name` varchar(128) DEFAULT NULL,
+  `projects_remark` varchar(1024) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `role`
+-- Dumping data for table `projects`
 --
 
-LOCK TABLES `role` WRITE;
-/*!40000 ALTER TABLE `role` DISABLE KEYS */;
-/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `roles_name_unique` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `translate_info`
+--
+
+DROP TABLE IF EXISTS `translate_info`;
+/*!50001 DROP VIEW IF EXISTS `translate_info`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `translate_info` (
+  `id` tinyint NOT NULL,
+  `info_id` tinyint NOT NULL,
+  `content` tinyint NOT NULL,
+  `infos_language` tinyint NOT NULL,
+  `translates_language` tinyint NOT NULL,
+  `result` tinyint NOT NULL,
+  `updated_at` tinyint NOT NULL,
+  `created_at` tinyint NOT NULL,
+  `deleted_at` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `translates`
@@ -98,15 +186,15 @@ DROP TABLE IF EXISTS `translates`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `translates` (
-  `translate_id` varchar(128) NOT NULL,
-  `info_id` varchar(128) DEFAULT NULL,
-  `translate_language` varchar(128) DEFAULT NULL,
-  `translate_result` text,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `info_id` int(11) DEFAULT NULL,
+  `language` varchar(128) DEFAULT NULL,
+  `result` text,
   `updated_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`translate_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,8 +203,38 @@ CREATE TABLE `translates` (
 
 LOCK TABLES `translates` WRITE;
 /*!40000 ALTER TABLE `translates` DISABLE KEYS */;
-INSERT INTO `translates` VALUES ('1413287190543d0d16792825.01343826','141068563354155ac1a9da31.91101597','cn','嗨','2014-10-14 03:51:08','2014-10-14 03:46:30','2014-10-14 03:51:08'),('1413287394543d0de2f14222.06357417','141068563354155ac1a9da31.91101597','cns','嗨','2014-10-14 03:49:54','2014-10-14 03:49:54',NULL),('1413287921543d0ff17601b1.38465458','141068563354155ac1a9da31.91101597','cns','嗨','2014-10-14 03:58:41','2014-10-14 03:58:41',NULL);
+INSERT INTO `translates` VALUES (1,1,'English','hello','2015-03-12 21:42:21','2015-03-12 21:41:04','2015-03-12 21:42:21');
 /*!40000 ALTER TABLE `translates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_role` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `assigned_roles_user_id_foreign` (`user_id`),
+  KEY `assigned_roles_role_id_foreign` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -127,21 +245,22 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(128) NOT NULL,
-  `user_nickname` varchar(1024) DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `nickname` varchar(1024) DEFAULT NULL,
   `password` varchar(1024) DEFAULT NULL,
-  `user_email` varchar(1024) DEFAULT NULL,
+  `email` varchar(1024) DEFAULT NULL,
   `remember_token` varchar(128) DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `user_location` varchar(45) DEFAULT NULL,
-  `user_translate` text,
-  `user_adout` text,
-  `user_sex` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_name_UNIQUE` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  `location` varchar(45) DEFAULT NULL,
+  `translate` text,
+  `about` text,
+  `sex` varchar(45) DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -150,9 +269,28 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'a','啊','$2y$10$GUfkA1AfmFljPg6nkTjQR.YPlT.4WW8UdbtxYYfBLwXbqM2uerXd2','a@ezgotranslate.com',NULL,NULL,NULL,'重庆','中文','关于我','男'),(2,'ff',NULL,'$2y$10$CrnlMtDf6QE6XtCkvvi/WeGSG9VEvaKwqjax.8Oq77KSMuW8IgSVK','das',NULL,'2014-07-14 05:25:06','2014-07-14 05:25:06',NULL,NULL,NULL,NULL),(3,'aaa',NULL,'$2y$10$GUfkA1AfmFljPg6nkTjQR.YPlT.4WW8UdbtxYYfBLwXbqM2uerXd2',NULL,NULL,'2014-10-14 04:12:27','2014-10-14 04:12:27',NULL,NULL,NULL,NULL),(4,'ada',NULL,'$2y$10$l.LVGIe3.eNlQTMcGRIMLu16WyS/1PpvxRq5O89oqDypR7YZJ61Hy',NULL,NULL,'2014-10-14 04:14:50','2014-10-14 04:14:50','四川','中文',NULL,'女'),(6,'adaaa','啊大','$2y$10$bFlADc85A2mvsU8r3tIBfOGSW8nCGyj/eyWcfOiuGFviPaC8paJuS',NULL,NULL,'2014-10-14 04:15:37','2014-10-14 04:15:37','四川','中文',NULL,'女');
+INSERT INTO `users` VALUES (1,'a','啊','$2y$10$GUfkA1AfmFljPg6nkTjQR.YPlT.4WW8UdbtxYYfBLwXbqM2uerXd2','a@ezgotranslate.com',NULL,NULL,NULL,'重庆','中文','关于我','男',NULL),(2,'ff',NULL,'$2y$10$CrnlMtDf6QE6XtCkvvi/WeGSG9VEvaKwqjax.8Oq77KSMuW8IgSVK','das',NULL,'2014-07-14 05:25:06','2014-07-14 05:25:06',NULL,NULL,NULL,NULL,NULL),(3,'aaa',NULL,'$2y$10$GUfkA1AfmFljPg6nkTjQR.YPlT.4WW8UdbtxYYfBLwXbqM2uerXd2',NULL,NULL,'2014-10-14 04:12:27','2014-10-14 04:12:27',NULL,NULL,NULL,NULL,NULL),(4,'ada',NULL,'$2y$10$l.LVGIe3.eNlQTMcGRIMLu16WyS/1PpvxRq5O89oqDypR7YZJ61Hy',NULL,NULL,'2014-10-14 04:14:50','2014-10-14 04:14:50','四川','中文',NULL,'女',NULL),(6,'adaaa','啊大','$2y$10$bFlADc85A2mvsU8r3tIBfOGSW8nCGyj/eyWcfOiuGFviPaC8paJuS',NULL,NULL,'2014-10-14 04:15:37','2014-10-14 04:15:37','四川','中文',NULL,'女',NULL),(7,'cdffh',NULL,'$2y$10$mBOg3iVS92vnZJ4etkBB1ul0nAAd99zTT47/bR49SyYPN.t49W7Qy','cdffh@126.com',NULL,'2015-03-04 22:51:12','2015-03-04 22:51:12',NULL,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `translate_info`
+--
+
+/*!50001 DROP TABLE IF EXISTS `translate_info`*/;
+/*!50001 DROP VIEW IF EXISTS `translate_info`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `translate_info` AS select `translates`.`id` AS `id`,`translates`.`info_id` AS `info_id`,`infos`.`content` AS `content`,`infos`.`language` AS `infos_language`,`translates`.`language` AS `translates_language`,`translates`.`result` AS `result`,`translates`.`updated_at` AS `updated_at`,`translates`.`created_at` AS `created_at`,`translates`.`deleted_at` AS `deleted_at` from (`translates` left join `infos` on((`translates`.`info_id` = `infos`.`id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -163,4 +301,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-01-02 16:55:20
+-- Dump completed on 2015-03-15 16:19:05
